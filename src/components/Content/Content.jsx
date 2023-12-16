f1f163642671f61c6bfeeebdaded63c964d84cb2
Magomed Terkhoev <hoower@mail.ru>
2023-12-16 15:56:16 +0300

    test commit

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,9 +1,10 @@
-import Header from "../Header/Header";
-import Navbar from "../Navbar/Navbar";
-import Footer from "../Footer/Footer";
-import Main from "../Main/Main";
-import style from "./Content.module.css";
-import { Outlet, Routes } from "react-router-dom";
+import Header from '../Header/Header';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+import Main from '../Main/Main';
+import style from './Content.module.css';
+import { Outlet, Routes } from 'react-router-dom';
+console.log('test');
 
 export default function Content() {
   return (
